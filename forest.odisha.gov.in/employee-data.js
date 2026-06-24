8edac490cdc9ef623b89529659b06f25c6eb04b1
jbskjokmth67 <premiumdata84@gmail.com>
2026-06-24 12:05:04 +0530

    Add files via upload

---
+++ forest.odisha.gov.in/employee-data.js
@@ -0,0 +1,35 @@
+// Local Database Matrix of Department Employees - Group 'C' Cadre
+const LOCAL_EMPLOYEE_DB = {
+    "FG202601": {
+        password: "K7B9X2",
+        name: "Shri Biswajit Moharana",
+        position: "Forest Guard (Group 'C' Non-Gazetted)",
+        cadre: "Odisha Sub-ordinate Forest Service (Territorial)",
+        division: "Kendrapara Mangrove Forest Division",
+        circle: "Bhubaneswar Forest Circle",
+        joiningDate: "24.06.2026",
+        venue: "Forest Range Office Jambu, Mahakalapada, Kendrapara, Odisha 754224",
+        photo: "Op84xqz.jpg", 
+        logs: [
+            { period: "June 2026 (Current)", days: "22 Working Days", hours: "176 Patrol Hours Logged" },
+            { period: "May 2026", days: "26 Working Days", hours: "208 Patrol Hours Logged" },
+            { period: "April 2026", days: "25 Working Days", hours: "200 Patrol Hours Logged" }
+        ]
+    },
+    "FG202602": {
+        password: "M4V1W9",
+        name: "Shri Lalata Jena",
+        position: "Forest Guard (Group 'C' Non-Gazetted)",
+        cadre: "Odisha Sub-ordinate Forest Service (Territorial)",
+        division: "Kendrapara Mangrove Forest Division",
+        circle: "Bhubaneswar Forest Circle",
+        joiningDate: "24.06.2026",
+        venue: "Forest Range Office Jambu, Mahakalapada, Kendrapara, Odisha 754224",
+        photo: "Q7xcr6.jpg", 
+        logs: [
+            { period: "June 2026 (Current)", days: "24 Working Days", hours: "192 Patrol Hours Logged" },
+            { period: "May 2026", days: "27 Working Days", hours: "216 Patrol Hours Logged" },
+            { period: "April 2026", days: "24 Working Days", hours: "192 Patrol Hours Logged" }
+        ]
+    }
+};
